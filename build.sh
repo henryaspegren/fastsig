e5943eea0a63f748f6d2e58af63b12ec7116da43
Henry Aspegren <henryaspegren@gmail.com>
2018-02-20 14:19:51 -0500

    build setup

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
-#! /bin/bash
+#!/bin/bash
+
+echo '---------------setting up fastsig for b_verify----------------------'
+
 
 mkdir src/generated
 protoc --java_out=src.generated proofs.proto
 
+echo '-----------installing fastsig for b_verify with maven--------------'
+mvn clean install
+
+echo '--------------------------completed--------------------------------'
